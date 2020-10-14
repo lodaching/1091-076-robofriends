68fae100ea05a3dc8937b053099d08209e958fe7
lodaching <lodachinglo@gmail.com>
2020-10-14 22:21:08 +0800

    Updates

---
+++ precache-manifest.3307296ad2551f21233cfc18f4697623.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0251b53a01e1d7718b31f71b286da1e",
+    "url": "/1091-076-robofriends/index.html"
+  },
+  {
+    "revision": "e9c97b84de84f0d8d725",
+    "url": "/1091-076-robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "72537b3e789d66cabc72",
+    "url": "/1091-076-robofriends/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e9c97b84de84f0d8d725",
+    "url": "/1091-076-robofriends/static/js/2.0b5b9267.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/1091-076-robofriends/static/js/2.0b5b9267.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72537b3e789d66cabc72",
+    "url": "/1091-076-robofriends/static/js/main.17278598.chunk.js"
+  },
+  {
+    "revision": "e5a46186bc19e229fbed",
+    "url": "/1091-076-robofriends/static/js/runtime-main.30305028.js"
+  }
+]);
